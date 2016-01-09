dc97f5833d7d7f8a81d5d605f27ca596ee0e48b9
ryichang <ryichang@gmail.com>
2016-01-09 01:50:06 -0800

    Incorporated Cordova Camera

--- myApp/www/js/controllers/photo.js
+++ myApp/www/js/controllers/photo.js
@@ -1,12 +1,41 @@
 'use strict';
 
-app.controller('PhotoCtrl', function($scope) {
+app.controller('PhotoCtrl', function($scope, $cordovaCamera, $rootScope, $state) {
   
+  var options = {
+      quality: 75,
+      destinationType: navigator.camera.DestinationType.DATA_URL,
+      allowEdit: true,
+      encodingType: navigator.camera.EncodingType.JPEG,
+      popoverOption: CameraPopoverOptions,
+      targetWidth: 500,
+      targetHeight: 500,
+      saveToPhotoAlbum: false
+    };
+
+
   $scope.takePhoto = function() {
     console.log('takePhoto was clicked');
-  }
+    //Camera Plugin
+    
+    options.sourceType = navigator.camera.PictureSourceType.CAMERA;
+    
+
+    $cordovaCamera.getPicture(options).then(function(photo){
+      $rootScope.imgURI = "data:image/jpeg;base64," +photo;
+      $state.go('tab.photo-detail');
+    });
+  };
 
   $scope.choosePhoto = function() {
     console.log('will link up to the photoLibrary');
-  }
+    
+    options.sourceType = navigator.camera.PictureSourceType.PHOTOLIBRARY;
+
+    $cordovaCamera.getPicture(options).then(function(photo){
+      $rootScope.imgURI = "data:image/jpeg;base64," +photo;
+      $state.go('tab.photo-detail');
+    });
+
+  };
 });
--- myApp/www/js/controllers/photoDetail.js
+++ myApp/www/js/controllers/photoDetail.js
@@ -1,16 +1,14 @@
 'use strict';
 
-app.controller('PhotoDetailCtrl', function($scope, Chats, Products) {
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+app.controller('PhotoDetailCtrl', function($scope, $state, Products, $rootScope) {
 
-  $scope.image='http://palazzoducale.visitmuve.it/wp-content/uploads/2013/01/Manet-02-web1.jpg';
+
+  $scope.imgURI = $rootScope.imgURI;
 
   $scope.button = function(product) {
-    console.log('button was clicked', product, $scope.image);
-    Products.saveProduct(product, $scope.image);
+    // console.log('button was clicked', product, $scope.image);
+    Products.saveProduct(product, $scope.imageURI).then(function() {
+    	$state.go('tab.dash');
+    });
   };
 });
